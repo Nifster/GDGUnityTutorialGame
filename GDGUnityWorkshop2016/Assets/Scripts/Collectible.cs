d58225b5afc66ec83f51fe2e349b96d18678b8aa
Hanif <h_ghazali@hotmail.com>
2016-08-25 00:55:34 +0800

    Implement pick-up and game manager

---
+++ GDGUnityWorkshop2016/Assets/Scripts/Collectible.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Attach this script to make an object into a collectible.
+
+/** 
+ * HOW IT WORKS:
+ * Note: Requires the GameManager.cs script to be attached to something in the scene.
+ * 
+ * This script makes it so that when something enters its hitbox, do
+ * 	1) Check if it is the player that entered. If not, return.
+ *  2) Tell the GameManager that an item has been collected.
+ *  3) Delete itself
+ */
+
+public class Collectible : MonoBehaviour {
+
+	private GameManager gameManager;
+
+    [SerializeField]
+    private string collectorName = "Player";
+
+	// Called on start
+	void Start() {
+		CheckForErrors();
+
+		gameManager = GameManager.instance;
+		if (gameManager != null)
+			gameManager.IncrementCollectibleCount();
+
+	}
+
+	// Called when some other object enters your hitbox. (note: isTrigger must be on!)
+	void OnTriggerEnter2D(Collider2D other) {
+		// First we need to check whether the thing it's colliding with is the player.
+		// There are a few methods to do this. I'll be using Method 1.
+
+		// Method 1: Check object name (I'm using this)
+		if (other.name.ToLower() != collectorName.ToLower()) return;
+
+		// Method 2: Check object tag (I'm not using this)
+		//if (other.tag != 7) return;
+
+		// Method 3: Check whether object has the script component "Controller" attached. (I'm not using this)
+		//var controller = other.GetComponent<Controller>();
+		//if(controller == null) return;
+
+		if (gameManager != null) {
+			gameManager.ItemCollected();
+		}
+		Destroy(this.gameObject);
+	}
+
+	// This function is not important.
+	// I made it just to diagnose some possible errors that you might make during the workshop :D
+	private void CheckForErrors() {
+		var colliders = this.GetComponents<Collider2D>();
+		if (colliders == null || colliders.Length == 0) {
+			Debug.Log ("NUSGDG: Attach a collider to your collectible!");
+		}
+		bool hasTriggerCollider = false;
+		foreach (var collider in colliders) {
+			if (collider.isTrigger)
+				hasTriggerCollider = true;
+		}
+		if (!hasTriggerCollider) {
+			Debug.Log ("NUSGDG: Set isTrigger (in the Box Collider 2D settings) to true for the pickup to work");
+		}
+	}
+}
---
+++ GDGUnityWorkshop2016/Assets/Scripts/GameManager.cs
@@ -0,0 +1,134 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// This is a game manager script. Attach it to something.
+
+/** 
+ * HOW IT WORKS:
+ * 
+ * The script does the following:
+ *
+ * ==ONE==
+ * Every X seconds, spawn a new prefabCollectible in the map.
+ *   - X is determined by the variable "spawnCooldown"
+ *
+ * ==TWO==
+ * Keep an integer variable named "score".
+ *   - The function "ItemCollected" is called from Collectible.cs whenever an item is collected.
+ *   - Whenever "ItemCollected" is called, increment score by 1.
+ *
+ * ==THREE==
+ * Draw a number indicating the score on the in-game GUI
+ * 
+ * ==FOUR==
+ * [Not Essential]
+ * Draw some white guide lines in the Unity Editor to indicate the spawn area box.
+ *
+ */
+
+public class GameManager : MonoBehaviour {
+
+	public static GameManager instance {get; private set;}
+
+	// NOTE: Anything with [SerializeField] is editable in the unity editor.
+
+	[SerializeField]
+	private GameObject prefabCollectible = null;
+
+	//[SerializeField]
+	private Rect scoreRect = new Rect(40,40,200,100);
+	
+	//[SerializeField]
+	private GUIStyle scoreGuiStyle;
+
+	private int score;
+
+	[SerializeField]
+	private Vector2 spawnLimitBottomLeft = new Vector2(-7,-4.5f);
+	[SerializeField]
+	private Vector2 spawnLimitTopRight = new Vector2(7, 4.5f);
+
+	private float nextSpawnTime;
+
+	[SerializeField]
+	private float spawnCooldown = 1f;
+
+	private int collectibleCount;
+	[SerializeField]
+	private int collectibleLimit = 10;
+
+	// Awake is called before start.
+	void Awake() {
+		instance = this;
+	}
+
+	// Use this for initialization
+	void Start () {
+		CheckForErrors();
+
+		score = 0;
+		nextSpawnTime = Time.time + spawnCooldown;
+	}
+
+	public void ItemCollected() {
+		score++;
+		collectibleCount--;
+	}
+
+    public void ModifyScore(int scoreChange) {
+        score += scoreChange;
+    }
+
+    public void IncrementCollectibleCount() {
+		collectibleCount++;
+	}
+
+	private void SpawnCollectible() {
+		float spawnX = Random.Range(spawnLimitBottomLeft.x, spawnLimitTopRight.x);
+		float spawnY = Random.Range(spawnLimitBottomLeft.y, spawnLimitTopRight.y);
+
+		Instantiate(prefabCollectible, new Vector3(spawnX, spawnY, 0), prefabCollectible.transform.rotation);
+	}
+
+	// Update() : Update is called once per frame
+	void Update() {
+		// Controls the spawning of collectibles
+		if (prefabCollectible != null && Time.time > nextSpawnTime) {
+			nextSpawnTime += spawnCooldown;
+
+			if (collectibleCount < collectibleLimit)
+				SpawnCollectible();
+		}
+	}
+
+	// OnGUI() : Use this for drawing things directly on to the screen.
+	void OnGUI() {
+		GUI.Label(scoreRect, score.ToString(), scoreGuiStyle);
+	}
+
+	// OnDrawGizmos() : This draws the white guidelines in the unity editor.
+	void OnDrawGizmos() {
+		Gizmos.DrawLine (new Vector3(spawnLimitBottomLeft.x, spawnLimitBottomLeft.y, 0),
+		                 new Vector3(spawnLimitBottomLeft.x, spawnLimitTopRight.y, 0));
+		Gizmos.DrawLine (new Vector3(spawnLimitBottomLeft.x, spawnLimitBottomLeft.y, 0),
+		                 new Vector3(spawnLimitTopRight.x, spawnLimitBottomLeft.y, 0));
+		Gizmos.DrawLine (new Vector3(spawnLimitTopRight.x, spawnLimitTopRight.y, 0),
+		                 new Vector3(spawnLimitBottomLeft.x, spawnLimitTopRight.y, 0));
+		Gizmos.DrawLine (new Vector3(spawnLimitTopRight.x, spawnLimitTopRight.y, 0),
+		                 new Vector3(spawnLimitTopRight.x, spawnLimitBottomLeft.y, 0));
+	}
+
+
+	// This function is not important.
+	// I made it just to diagnose some possible errors that you might make during the workshop :D
+	void CheckForErrors() {
+		// Initialise font style if not set.
+		if (scoreGuiStyle == null) {
+			scoreGuiStyle = new GUIStyle ();
+			scoreGuiStyle.fontSize = 20;
+			scoreGuiStyle.normal.textColor = Color.white;
+		}
+		if (prefabCollectible == null)
+			Debug.Log ("NUSGDG: The GameManager's collectible prefab has not been assigned! The GameManager can't spawn any collectibles.");
+	}
+}
